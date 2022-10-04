0a9432d363c28cb07979dbefa9a5146e4c467464
Rob-Mundy <robertemundy@googlemail.com>
2022-10-04 09:58:36 +0000

    Add event listeners for page load and test buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,27 @@
-function DOMcontentloaded() {
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
 
-}
+    for(let button of buttons) {
+        button.addEventListener("click", function() {
+            if(this.getAttribute("data-type") === "submit") {
+                alert("Let's play!");
+            } else {
+                let userSelection = this.getAttribute("data-type");
+                alert(`You selected ${userSelection}`)
+            }
+        })
+    }
+})
 
 function runGame() {
 
 }
 
-function checkAnswer() {
+function determineWinner() {
 
 }
+
+//function checkAnswer() {}
 
 function incrementWins() {
 
