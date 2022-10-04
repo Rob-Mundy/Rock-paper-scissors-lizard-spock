5f01df44f0c147497f625b27fcab895f18b56def
Rob-Mundy <robertemundy@googlemail.com>
2022-10-04 16:49:19 +0000

    Roll back to earlier commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,13 +11,41 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         })
     }
+
+    runGame();
 })
 
 function runGame() {
 
+    // create array of possible answers, assign user's selection 
+    // and create random computer answer
+
+    let handShapes = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+    let computerShape = handShapes[Math.floor(Math.random() * handShapes.length)];
+
+        let userShape;
+        let userButtons = document.getElementsByClassName('user-selection-zone')[0];
+        let buttons = userButtons.getElementsByTagName('button');
+        for(let button of buttons) {
+        button.addEventListener("click", function() {
+            userShape = this.getAttribute("data-type");
+            console.log(userShape);
+        }); 
+        
+        }
+        
+    //displayBattle(userShape, computerShape);
+    console.log(`You chose ${userShape}`);
+    console.log(`Computer chose ${computerShape}`);
 }
 
-function determineWinner() {
+function displayBattle(userSelection,computerSelection) {
+
+    document.getElementById('userSelection').textContent = userSelection;
+    document.getElementById('computerSelection').textContent = computerSelection;
+}
+
+function determineWinner(userShape, computerShape) {
 
 }
 
